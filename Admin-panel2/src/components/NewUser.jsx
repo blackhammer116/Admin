0f6a977b303262d743e6e9d80e68dbdcaa9aab57
blackhammer <coder@gmail.com>
2024-07-15 20:53:06 +0300

    fixed newUser comp

--- Admin-panel2/src/components/NewUser.jsx
+++ Admin-panel2/src/components/NewUser.jsx
@@ -1,6 +1,7 @@
 // 'use client';
 
 import { Divider, TextInput } from '@tremor/react';
+import {useState} from 'react'
 import {Link} from 'react-router-dom'
 import axios from 'axios'
 
@@ -21,7 +22,7 @@ export default function NewUser() {
         e.preventDefault();
       
         try {
-          await axios.post('http://127.0.0.1:5000/client_signin', formData);
+          await axios.post('http://127.0.0.1:5000/signup', formData);
           // Reset the form data after successful submission
           setFormData({ 
             f_name: '',
@@ -50,7 +51,7 @@ export default function NewUser() {
         <p className="mt-1 text-tremor-default leading-6 text-tremor-content dark:text-dark-tremor-content">
           fill the neccessary information
         </p>
-        <form action="/client_signin" onSubmit={handleSubmit} method="post" className="mt-8">
+        <form  onSubmit={handleSubmit} method="post" className="mt-8">
           <div className="grid grid-cols-1 gap-x-4 gap-y-6 sm:grid-cols-6">
             <div className="col-span-full sm:col-span-3">
               <label
@@ -192,6 +193,7 @@ export default function NewUser() {
                 password
               </label>
               <TextInput
+	        type="password"
                 id="password"
                 name="password"
                 placeholder="password"
@@ -203,7 +205,6 @@ export default function NewUser() {
             </div>
           </div>
           <Divider />
-        </form>
         <div className="flex items-center justify-end space-x-4">
             <button
               type="button"
@@ -211,15 +212,14 @@ export default function NewUser() {
             >
               Cancel
             </button>
-            <Link to={'/users'}>
             <button
               type="submit"
               className="whitespace-nowrap rounded-tremor-default bg-tremor-brand px-4 py-2.5 text-tremor-default font-medium text-tremor-brand-inverted shadow-tremor-input hover:bg-tremor-brand-emphasis dark:bg-dark-tremor-brand dark:text-dark-tremor-brand-inverted dark:shadow-dark-tremor-input dark:hover:bg-dark-tremor-brand-emphasis"
             >
               Submit
             </button>
-            </Link>
           </div>
+        </form>
       </div>
     </>
   );
